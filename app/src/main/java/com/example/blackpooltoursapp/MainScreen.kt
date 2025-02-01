21f5a5ed0de98f91667422d1199cc8b70952dd62
Ihs4nn <ihsan.fikry@vodafone.com>
2025-02-01 15:55:16 +0000

    Added Mint green colour to button

--- app/src/main/java/com/example/blackpooltoursapp/MainScreen.kt
+++ app/src/main/java/com/example/blackpooltoursapp/MainScreen.kt
@@ -4,10 +4,14 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.ui.graphics.Color
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
@@ -17,9 +21,10 @@ fun MainScreen() {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Button(onClick = {
-            // Navigate to LoginScreen.kt
-        }) {
+        Button(
+            onClick = { /* TODO: Add navigation to LoginScreen.kt */ },
+            colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFA8E6CF))
+        ) {
             Text("Login")
         }
     }

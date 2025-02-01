e0a25f0f52a14bf982c825e7ae5e721c47abe96d
Ihs4nn <ihsan.fikry@vodafone.com>
2025-02-01 17:24:21 +0000

    Added logo image

--- app/src/main/java/com/example/blackpooltoursapp/MainScreen.kt
+++ app/src/main/java/com/example/blackpooltoursapp/MainScreen.kt
@@ -1,17 +1,17 @@
 package com.example.blackpooltoursapp
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
+import androidx.compose.ui.res.painterResource
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.ui.graphics.Color
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
@@ -21,6 +21,10 @@ fun MainScreen() {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Image(
+            painter = painterResource(id = R.drawable.bptlogo),
+            contentDescription = "BlackPool Tours Logo"
+        )
         Button(
             onClick = { /* TODO: Add navigation to LoginScreen.kt */ },
             colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFA8E6CF))
@@ -28,6 +32,8 @@ fun MainScreen() {
             Text("Login")
         }
     }
+
+
 }
 
 @Preview(showBackground = true)

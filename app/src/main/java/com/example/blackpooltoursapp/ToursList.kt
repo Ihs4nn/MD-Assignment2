104906cae553ebfbcf1ac4f0e325684d93c2cd70
Ihs4nn <ihsan.fikry@vodafone.com>
2025-02-03 16:56:06 +0000

    Adding list of tours

--- app/src/main/java/com/example/blackpooltoursapp/ToursList.kt
+++ app/src/main/java/com/example/blackpooltoursapp/ToursList.kt
@@ -1,5 +1,6 @@
 package com.example.blackpooltoursapp
 
+import android.media.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -11,5 +12,21 @@ import androidx.compose.material3.Text
 
 @Composable
 fun ToursList(navController: NavController) {
-    Text(text="Hi!, you made it this far")
+    data class Tours(
+        val image: Int,
+        val title: String,
+        val desc: String,
+        val extradesc: String,
+        val isSelected: Boolean = false
+    )
+
+    val sample_tours = listOf(
+        Tours(R.drawable.spain, "Sagrada Famillia", "A breathtaking church in Barcelona","Lorem ipsum dolor sit amet, consectetur adipiscing elit"),
+        Tours(R.drawable.italy, "Rome","The capital of Italy","Lorem ipsum dolor sit amet, consectetur adipiscing elit"),
+        Tours(R.drawable.aussie, "Kangaroo Islam","A wildlife paradise in Australia","Lorem ipsum dolor sit amet, consectetur adipiscing elit"),
+        Tours(R.drawable.japan, "Mount Fuji", "Japan's highest mountain","Lorem ipsum dolor sit amet, consectetur adipiscing elit")
+    )
+
+
 }
+

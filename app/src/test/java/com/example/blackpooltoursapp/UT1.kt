23187c14bf31ea8889d4ae238893641ec074f75e
Ihs4nn <ihsan.fikry@vodafone.com>
2025-02-17 11:09:03 +0000

    First code of UT1, havent ran yet

--- app/src/test/java/com/example/blackpooltoursapp/UT1.kt
+++ app/src/test/java/com/example/blackpooltoursapp/UT1.kt
@@ -1,2 +1,23 @@
 package com.example.blackpooltoursapp
 
+import org.junit.Test
+import org.junit.Assert.*
+
+class UT1 {
+
+    @Test
+    fun addTourToSavedTours(){
+        // Step 1
+        val testViewModel = TourViewModel()
+        // Step 2
+        val testTour = Tour(
+            image = 0,
+            title = "testCity",
+            desc = "testDesc",
+            extraInfo = "testExtraInfo"
+        )
+        // Step 3
+        testViewModel.addTour(testTour)
+        assertTrue(testViewModel.savedTours.contains(testTour))
+    }
+}
